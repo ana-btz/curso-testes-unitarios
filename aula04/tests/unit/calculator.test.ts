82e7d2413586cfecd0044349feed41305bf331a2
Ana Beatriz <silva.anabtz@gmail.com>
2023-10-04 08:22:50 -0300

    exercicio aula 04

--- aula04/tests/unit/calculator.test.ts
+++ aula04/tests/unit/calculator.test.ts
@@ -1,5 +1,30 @@
+import calculator from "calculator";
+
 describe("calculator tests", () => {
-  it("should work", async () => {
-    expect(true).toBe(true);
+  it("should sum two numbers", async () => {
+    const int = calculator.sum(2, 2);
+    const intNeg = calculator.sum(-1, -2);
+    const dec = calculator.sum(1.5, 0.9);
+    const decNeg = calculator.sum(-2.1, -0.1);
+
+    expect(int).toBe(4);
+    expect(intNeg).toBe(-3);
+    expect(dec).toBe(2.4);
+    expect(decNeg).toBe(-2.2);
+  });
+
+  it("should subtratct two numbers", async () => {
+    const result = calculator.sub(1, 2);
+    expect(result).toBe(-1);
+  });
+
+  it("should divide two numbers", async () => {
+    const result = calculator.div(1, 2);
+    expect(result).toBe(0.5);
+  });
+
+  it("should multiply two numbers", async () => {
+    const result = calculator.mul(1, 2);
+    expect(result).toBe(2);
   });
 })
